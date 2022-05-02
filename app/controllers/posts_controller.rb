87c70cdbf31c24ccbce1b343d2b962d9293ea4ef
bonbon0202 <bonbon0202@gmail.com>
2022-05-03 02:30:24 +0900

    feat: add sorting, searching, pagination querystring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,17 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show update destroy ]
 
   def index
-    @posts = Post.all
+    sort = params[:sort] || 'created_at DESC'
+    category = params[:category] || 'title'
+    search_text = params[:search_text] || ''
+    page = params[:page] || 1
+    limit = params[:limit] || 10
+  
+    @posts = Post.order(sort).where(category + " LIKE ?", category == "title" ? "%#{params[:search_text]}%" : params[:search_text])
+    # TODO : max_page 추가
+    # max_page = (@posts.length() / limit.to_i).floor + 1
+    
+    @pagy, @posts = pagy(@posts, items: limit)
 
     render json: @posts, only: [:id, :title, :content, :writer]
   end
